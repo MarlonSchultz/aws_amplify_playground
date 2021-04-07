99011fc94d19a505281370a4ca43dd596219930f
Marlon <14779331+MarlonSchultz@users.noreply.github.com>
2021-04-07 12:20:03 +0200

    amplify base setup

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <amplify-authenticator>
+    <h1>This is being shown once athenticated?</h1>
+    <amplify-sign-out></amplify-sign-out>
+  </amplify-authenticator>
+</template>
+<script>
+
+
+</script>
